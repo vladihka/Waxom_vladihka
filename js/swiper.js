9e6df34b9f947db35896bb2e97350593d26dc1ef
vladihka <64697319+vladihka@users.noreply.github.com>
2021-04-01 18:27:16 +0200

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,186 @@
+new Swiper('.image-slider', {
+	// //стрелки
+	// navigation: {
+	// 	nextEl: '.swiper-button-next',
+	// 	prevEl: '.swiper-button-prev'
+	// },
+	// //навигация
+	// pagination: {
+	// 	el: '.swiper-pagination',
+	// 	//буллеты
+	// 	// clickable: true,
+	// 	// //динамические беллеты
+	// 	// dynamicBullets: true,
+	// 	// //кастомные буллеты
+	// 	// renderBullet: function (index, className) {
+	// 	// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+	// 	// },
+	// 	//фракция
+	// 	// type: 'fraction',
+	// 	// renderFraction: function (currentClass, totalClass) {
+	// 	// 	return 'Фото <span class="' + currentClass + '"></span>' + ' из ' + '<span class="' + totalClass + '"></span>';
+	// 	// },
+	// 	//прогресбар
+	// 	//type: 'progressbar'
+
+
+	// },
+	// //скролл
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// 	//возможность перетаскивать скролл
+	// 	draggable: true
+	// },
+	// //включение / выключение перетаскивания на ПК
+	// simulateTouch: true,
+	// // чуствителиность свайпа
+	// touchRatio: 1,
+	// // угол срабатывания свайпа/перетаскивания
+	// touchAngle: 45,
+	// // курсрос перетаскивания
+	// grabCursor: true,
+	// //переключение при клике на слайд
+	// slideToClickedSlide: true,
+
+	// //навигация по хешу
+	// hashNavigation: {
+	// 	//отслеживать состояние
+	// 	watchState: true,
+	// },
+
+	// //управление клавиатурой
+	// keyboard: {
+	// 	//включить / выключить
+	// 	enabled: true,
+	// 	//включить/выключить только когда слайдер в пределах вьюпорта
+	// 	onlyInViewporp: true,
+	// 	//включить/выключить управление клавишами pageUp, pageDown
+	// 	pageUpDown: true
+	// },
+
+	// //управление колесом мыши
+	// mousewheel: {
+	// 	//чуствительность колеса мыши
+	// 	sensitivity: 1,
+	// 	//класс обьекта на котором будет срабатывать прокрутка мыштю
+	// 	eventsTarget: ".image-slider"
+	// },
+
+	// //автовысота
+	// autoHeight: true,
+
+	// //количество слайдов для показа
+	// slidesPerView: 1,
+
+	// //отключение функцианала если слайдов меньше чем нужно
+	// watchOverflow: true,
+
+	// //отступ между слайдами
+	// spaceBetween: 30,
+
+	// //количество пролистуемых слайдов
+	// slidesPerGroup: 1,
+
+	// //активный слайд по центру
+	// //centerSlides: true,
+
+	// //стартовый слайд
+	// initialSlide: 1,
+
+	// //мультирядность
+	// //slidesPerColumn: 2,
+
+	// //бесконечный слайдер
+	// loop: false,
+
+	// //свободный режим
+	// freeMode: true,
+
+	//автопрокрутка
+	autoplay: {
+		//пауза между прокруткой
+		delay: 1000,
+		//закончить на последнем слайде
+		stopLastSlide: true,
+		//отключить после ручного переключения
+		disableOnInteraction: false
+	},
+
+	// //скорость
+	speed: 2000,
+
+	// //вертикальный слайдер
+	// //direction: 'vertical',
+
+	// //листание
+	// // effect: 'fade',
+
+	// // //дополнение к fade
+	// // fadeEffect: {
+	// // 	//паралелиная смена прозрачности
+	// // 	crossFade: true
+	// // },
+
+	// //переворот
+	// // effect: 'flip',
+
+	// // //дополнение к flip
+	// // flipEffect: {
+	// // 	//тень
+	// // 	slideShadow: true,
+	// // 	//показ только активного слайда
+	// // 	limitRotation: true
+	// // },
+
+	// //куб
+	// // effect: 'cube',
+	// // //дополнение к cube
+	// // cubeEffect: {
+	// // 	//найстройки тени
+	// // 	slideShadows: true,
+	// // 	shadow: true,
+	// // 	shadowOffset: 20,
+	// // 	shadowScale: 0.94
+	// // },
+
+	// //ефект потока
+	// // efect: 'coverflow',
+	// // //дополнение к coverflow
+	// // coverflowEffect: {
+	// // 	//угол
+	// // 	rotate: 20,
+	// // 	//наложение
+	// // 	stretch: 50,
+	// // 	//тень
+	// // 	slideShadows: true
+	// // },
+
+	// //braekpoints
+	// breakpoints: {
+	// 	320: {
+	// 		slidesPerView: 1,
+	// 	},
+	// 	480: {
+	// 		slidesPerView: 2,
+	// 	},
+	// 	992: {
+	// 		slidesPerView: 3,
+	// 	}
+	// },
+});
+
+
+{/* < div class="image-slider__image" >
+	<div class="header_bottom_info">
+		<div class="header_bottom_title header_bottom_title_1">Unique and Modern Design</div>
+		<div class="header_bottom_subtitle">Portfolio PSD Template</div>
+		<div class="header_bottom_text">Nam liber tempor cum soluta nobis eleifend option
+		congue nihil
+									imperdiet doming id quod mazim placerat facer possim assum.</div>
+		<div class="header_bottom_button">
+			<button>Get Started</button>
+		</div>
+	</div>
+						</ > */}
+
+
